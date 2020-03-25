318f2a67733e914cb9c676e86edd989552d31587
bustling-clone-9138 <jtaylorjohnson@gmail.com>
2020-03-25 22:50:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
-  if array.count = 1
+  if array.count == 1
     return array
-  elsif array.count = 2
+  elsif array.count == 2
     return array.join(" and ")
   else
-    array[-1] = "and #{array[-1]}"
+    array[-1] == "and #{array[-1]}"
     return array.join(", ")
   end
 end
