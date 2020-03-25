ae627f726c92b5b38aed7f45019a56fbec65a37a
bustling-clone-9138 <jtaylorjohnson@gmail.com>
2020-03-25 22:27:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,8 +9,3 @@ def oxford_comma(array)
   end
 end
 
-
-'''returns a string without any additional fomatting when given a 1-element array
-  adds 'and' between elements when given a 2-element array (FAILED - 1)
-  adds commas plus a final 'and' when given a 3-element array (FAILED - 2)
-  correctly formats arrays of lengths greater than three (FAILED - 3)
