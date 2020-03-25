fb9ac47aae2e960df0cbc3b154e7c3a0683a0cd1
bustling-clone-9138 <jtaylorjohnson@gmail.com>
2020-03-25 22:10:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
 def oxford_comma(array)
-array.count = 2 ? array.join("and")
+  if array.count = 2
+    array.join("and")
+  elsif arr
 end
 
 
-returns a string without any additional fomatting when given a 1-element array
+'''returns a string without any additional fomatting when given a 1-element array
   adds 'and' between elements when given a 2-element array (FAILED - 1)
   adds commas plus a final 'and' when given a 3-element array (FAILED - 2)
   correctly formats arrays of lengths greater than three (FAILED - 3)
